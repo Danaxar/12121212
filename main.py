5c9b61060ee4377bc281bad72a4f24d678ea0278
Daniel <daniel.catalan@usach.cl>
2022-08-24 19:15:13 +0200

    feat: kill process

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ Initialiced: 24-08-2022
 import Config as cfg
 import system as sys
 import time as t
-#import os
+import psutil
+
 
 print("Opening program...")
 myConfig = cfg.Config()
@@ -17,8 +18,8 @@ print("Programs to close: variable.programsToClose")
 print("App running.")
 while True:
     for programName in myConfig.programsToClose:
-        if sys.detectProgram(programName):
-            print(programName, "is running.")
-        else:
-            print(programName, "is not running")
+        for process in psutil.process_iter():
+            if programName == process.name():
+                process.kill()
+                print(programName, " closed.")
         t.sleep(2.0)
